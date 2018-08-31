38e6aafe57a847748d6aa57c511e2b018a9bd14c
Michael Keller <michaelkeller03@gmail.com>
2018-08-31 15:17:32 -0500

    added subscribe popup

--- js/settings.js
+++ js/settings.js
@@ -2,6 +2,8 @@ for(coaster in coasterData){
 	$("#coasterList").append('<div class="list-group"><a class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">'+coasterData[coaster]['Rollercoaster Name']+'</h5><small><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></small></div><input type="range" min="0" max="120" value="60" class="slider" id="'+coasterData[coaster]['Rollercoaster Name Short']+'" step="5" oninput="sliderChange(this)"><p id="'+coasterData[coaster]['Rollercoaster Name Short']+'-Number">60 Minutes</p></a></div>');
 }
 
+$("#footer").slideUp()
+
 
 var selectedCoasters = []
 
@@ -23,3 +25,17 @@ function addCoaster(data){
 function sliderChange(coaster){
 	$("#"+coaster.id+"-Number").text(coaster.value+' minutes');
 }
+
+function sleep(milliseconds) {
+	var start = new Date().getTime();
+	for (var i = 0; i < 1e7; i++) {
+	  if ((new Date().getTime() - start) > milliseconds){
+		break;
+	  }
+	}
+  }
+
+$('input[type=checkbox]').click(function() {
+	$("#footer").slideDown(1000);
+	$("#footer").slideUp(1000);
+});
