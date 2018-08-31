835c767ac32fa018d714ff2383583ccb08258c90
Michael Keller <michaelkeller03@gmail.com>
2018-08-31 15:02:30 -0500

    added slider for times on settings page

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
 for(coaster in coasterData){
-	$("#coasterList").append('<a class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">'+coasterData[coaster]['Rollercoaster Name']+'</h5><div class="switch"><input name="'+coasterData[coaster]['Rollercoaster Name']+'" type="checkbox" onclick="addCoaster(this)"><label for="cmn-toggle-4"></label></div></div></a>');
+	$("#coasterList").append('<div class="list-group"><a class="list-group-item list-group-item-action flex-column align-items-start"><div class="d-flex w-100 justify-content-between"><h5 class="mb-1">'+coasterData[coaster]['Rollercoaster Name']+'</h5><small><input class="form-check-input" type="checkbox" value="" id="defaultCheck1"></small></div><input type="range" min="0" max="120" value="60" class="slider" id="'+coasterData[coaster]['Rollercoaster Name Short']+'" step="5" oninput="sliderChange(this)"><p id="'+coasterData[coaster]['Rollercoaster Name Short']+'-Number">60 Minutes</p></a></div>');
 }
 
 
@@ -18,4 +18,8 @@ function addCoaster(data){
 			}
 		}
 	}
+}
+
+function sliderChange(coaster){
+	$("#"+coaster.id+"-Number").text(coaster.value+' minutes');
 }
